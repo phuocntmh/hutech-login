660fcee18272e66bea55ec48b3b64bac459a4304
Nguyễn Trọng Minh Hồng Phước <39150891+phuocntmh@users.noreply.github.com>
2019-05-10 00:01:44 +0700

    Done

---
+++ HUTECH/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HUTECH.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string username, string password)
+        {
+            if (username=="1511060249" && password=="123456")
+                return RedirectToAction("Index", "Home");
+            ModelState.AddModelError("","Tài khoàn không đúng, vui lòng thử lại!");
+            return View("Index");
+        }
+    }
+}
